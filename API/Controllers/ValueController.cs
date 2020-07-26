1c5a4d600f3f7eb5f0b0801e01050cc0fcd641f8
krishnamishra11 <krishnamishra11@rediffmail.com>
2020-07-26 21:53:42 +0530

    first commit

---
+++ API/Controllers/ValueController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Percistent;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValueController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ValueController> _logger;
+
+
+        public DataContext _dbContext { get; }
+
+        public ValueController(ILogger<ValueController> logger, DataContext dbContext)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var valslist= await _dbContext.Values.ToListAsync();
+            return Ok(valslist);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Value>> Get(int Id)
+        {
+            var val= await _dbContext.Values.FindAsync(Id);
+            return Ok(val);
+        }
+    }
+}
---
+++ Percistent/DataContext.cs
@@ -0,0 +1,25 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Percistent
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions dbContextOptions ):base(dbContextOptions)
+        {
+            
+        }
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating( ModelBuilder modelBuilder   )
+        {
+            modelBuilder.Entity<Value>().HasData(
+                 new Value{ Id=1,Name="Value 101" });
+            modelBuilder.Entity<Value>().HasData(
+                 new Value{ Id=2,Name="Value 102" });
+            modelBuilder.Entity<Value>().HasData(
+                 new Value{ Id=3,Name="Value 103" });                                  
+        }
+
+    }
+}
